8b56a34de1fa493f542f71679b3b5872b37fa3ac
Hunter Ellis <elliswhunter@gmail.com>
2026-04-05 22:03:16 -0400

    remove markdown renderer

--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -12,18 +12,6 @@ return {
     end,
   },
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      file_types = { "markdown", "Avante" },
-      latex = { enabled = false },
-      completions = { blink = { enabled = true } },
-    },
-    ft = { "markdown", "Avante" },
-    config = function()
-      vim.keymap.set("n", "<leader>md", require("render-markdown").toggle, { desc = "Toggle markdown render" } )
-    end,
-  },
-  {
   "sheerun/vim-polyglot",
     lazy = false,
     init = function()
